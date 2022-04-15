f740a2c81acdcb8d305aeca91441ff38d46939cd
bbhushan99 <100056888+BharatBhushan99@users.noreply.github.com>
2022-04-15 05:29:03 +0000

    Added recursion.

--- main.py
+++ main.py
@@ -15,29 +15,34 @@ def divide(n1, n2):
     return n1 / n2
   else:
     return None
+def calculator():
+  should_continue = True
 
-should_continue = True
+  operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
 
-operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
+  num1 = float(input("What is the first number? "))
 
-num1 = float(input("What is the first number? "))
+  while should_continue:
 
-while should_continue:
-
- 
-  for operator in operations:
-    print(operator)
-
-  operation = input("Pick an operation: ")
-  num2 = float(input("What is the next number? "))
-
-  answer = operations[operation](num1,num2)
-
-  print(f"{num1} {operation} {num2} = {answer}")
    
-  if input("Type 'y' to continue {answer}, or type 'n' to start a new calculation:") == 'y':
-    num1 = answer
-  else:
-    should_continue = false
+    for operator in operations:
+      print(operator)
 
+    operation = input("Pick an operation: ")
+    num2 = float(input("What is the next number? "))
+
+    answer = operations[operation](num1,num2)
+
+    print(f"{num1} {operation} {num2} = {answer}")
+     
+    if input("Type 'y' to continue {answer}, or type 'n' to start a new calculation:") == 'y':
+      num1 = answer
+    else:
+      should_continue = False
+      calculator()
+ 
+
+calculator()
+
+    
 
